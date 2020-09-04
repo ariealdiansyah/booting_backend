351b69eb9aedb0b6402141dc3f70bde12b34fcf9
Axezious <ariealdiansyah98@gmail.com>
2020-09-04 14:55:15 +0700

    jasper report

--- src/main/java/com/lawencon/booting/dao/ReportDaoImpl.java
+++ src/main/java/com/lawencon/booting/dao/ReportDaoImpl.java
@@ -30,35 +30,20 @@ public class ReportDaoImpl extends BaseDao implements ReportDao {
 
 	@Override
 	public List<ReportTotalTicketAgent> getReportTotalTicketAgent(List<String> listCompany) throws Exception {
-		List<Object[]> aaData = em.createQuery("SELECT tkt.idCustomer.name as customers_name, tkt.idCustomer.idCompany.name as companies_name, count(tkt.id) as total_ticket FROM Tickets tkt WHERE tkt.idCustomer.idCompany.id IN (:listCompany) GROUP BY customers_name, companies_name", Object[].class)
-				.setParameter("listCompany", listCompany)
-				.getResultList();
-		
-		List<Object[]> aaaData = em.createQuery("SELECT tkt.idCustomer.name as customers_name, tkt.idCustomer.idCompany.name as companies_name, count(tkt.id) as total_ticket FROM Tickets tkt WHERE tkt.idCustomer.idCompany.id IN (:listCompany) AND tkt.idStatus.code = 'CL' GROUP BY customers_name, companies_name", Object[].class)
+		List<Object[]> aaData = em.createQuery("SELECT tkt.idCustomer.name as customers_name, tkt.idCustomer.idCompany.name as companies_name, count(tkt.id) as total_ticket, count(sts.id) as solved_ticket FROM Tickets tkt LEFT JOIN Status sts ON sts.id = tkt.idStatus AND sts.code = 'CL' WHERE tkt.idCustomer.idCompany.id IN (:listCompany) GROUP BY customers_name, companies_name", Object[].class)
 				.setParameter("listCompany", listCompany)
 				.getResultList();
 		
 		List<ReportTotalTicketAgent> listData = new ArrayList<>();
-		List<ReportTotalTicketAgent> listDataa = new ArrayList<>();
+		
 		aaData.forEach(l -> {
 			ReportTotalTicketAgent data = new ReportTotalTicketAgent();
 			data.setCustomers_name((String) l[0]);
 			data.setCompanies_name((String) l[1]);
 			data.setTotal_ticket((Long) l[2]);
+			data.setSolved_ticket((Long) l[3]);
 			listData.add(data);
 		});
-		aaaData.forEach(l -> {
-			ReportTotalTicketAgent data = new ReportTotalTicketAgent();
-			data.setSolved_ticket((Long) l[2]);
-			listDataa.add(data);
-		});
-		for(int i = 0; i < listDataa.size(); i++) {
-//			ReportTotalTicketAgent data = new ReportTotalTicketAgent();
-//			data = listData.get(i);
-//			data.setSolved_ticket(listDataa.get(i).getSolved_ticket());
-			listData.get(i).setSolved_ticket(listDataa.get(i).getSolved_ticket());
-		}
-			
 		return listData;
 	}
 }
