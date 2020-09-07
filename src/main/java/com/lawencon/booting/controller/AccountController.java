f567c08bc9609d1526618644a1a6c6324f9d142f
daimen <berlian.deni7@gmail.com>
2020-09-07 10:46:30 +0700

    kring

--- src/main/java/com/lawencon/booting/controller/AccountController.java
+++ src/main/java/com/lawencon/booting/controller/AccountController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lawencon.booting.model.Accounts;
 import com.lawencon.booting.service.AccountsService;
@@ -31,20 +30,21 @@ public class AccountController {
 	
 	@PostMapping("/insert")
 	public ResponseEntity<?> insert(@RequestBody String data) {
+		Accounts acc = new Accounts();
 		try {
-			Accounts acc = new ObjectMapper().readValue(data, Accounts.class);
+			acc = new ObjectMapper().readValue(data, Accounts.class);
 			acc = accountService.insert(acc);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>("Error : " + e.getMessage(), HttpStatus.BAD_GATEWAY);
 		}
-		String respon = "";
-		try {
-			respon = new ObjectMapper().writeValueAsString("Admin berhasil ditambah");
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(respon, HttpStatus.CREATED);
+//		String respon = "";
+//		try {
+//			respon = new ObjectMapper().writeValueAsString("Admin berhasil ditambah");
+//		} catch (JsonProcessingException e) {
+//			e.printStackTrace();
+//		}
+		return new ResponseEntity<>(acc, HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/all")
@@ -117,5 +117,18 @@ public class AccountController {
 		}
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@PutMapping("/forgot")
+	public ResponseEntity<?> forgotPass(@RequestBody String data){
+		Accounts acc = new Accounts();
+		try {
+			acc = new ObjectMapper().readValue(data, Accounts.class);
+			acc = accountService.forgotPass(acc);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Error : " + e.getMessage(), HttpStatus.BAD_GATEWAY);
+		}
+		return new ResponseEntity<>(acc, HttpStatus.OK);
 	}
 }
